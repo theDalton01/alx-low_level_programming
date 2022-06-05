b05ac920d51c06884a4e021d68a04400ab11a46e
theDalton01 <dabeejay691@gmail.com>
2022-06-05 12:35:48 -0700

    C variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * To print all numbers in base sixteen in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = o; num <= 10; num++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
