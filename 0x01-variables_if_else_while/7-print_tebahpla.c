71e727d260db9c460708c8bee2f753c15c44ba3e
theDalton01 <dabeejay691@gmail.com>
2022-06-05 11:46:28 -0700

    C variables

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * To print the alphabets in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	if (letter = 'z'; letter >= 'a';)
+		putchar(letter);
+
+	puchar('\n');
+	return (0);
+}
+
