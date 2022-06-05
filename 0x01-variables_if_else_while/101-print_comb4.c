a170dd8dfe8857ba74d05ce1f593f8980af524c9
theDalton01 <dabeejay691@gmail.com>
2022-06-05 13:53:30 -0700

    C variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
+ * Prints all possible combinations of three different digits
  * Return Always 0 (Success)
  */
 int main(void)
@@ -18,7 +18,7 @@ int main(void)
 				putchar((digit2 % 10) + '0');
 				putchar((digit3 % 10) + '0');
 
-				if (digit1 ==7 && digit2 == 8 && digit3 == 9)
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
 
 				putchar(',');
