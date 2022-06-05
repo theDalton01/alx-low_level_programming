8f4d787fbb3e16d3b3657c6136110154d1420069
theDalton01 <dabeejay691@gmail.com>
2022-06-05 12:50:02 -0700

    C variables

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,8 +10,8 @@ int main(void)
 	int num;
 	char letter;
 
-	for (num = 0; num <= 9; num++)
-		putchar(num);
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
 
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
