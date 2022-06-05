eab941139ddb13b35f859e111f571460b4d381e6
theDalton01 <dabeejay691@gmail.com>
2022-06-05 13:27:09 -0700

    C variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Emtry point
+ * Prints all possible sngle-digits combinations
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <=9; num++)
+		putchar((num % 10) + '0');
+	if (num == 9)
+		continue;
+	putchar(',');
+	putchar('');
+
+	putchar('\n');
+	return (0);
+}
