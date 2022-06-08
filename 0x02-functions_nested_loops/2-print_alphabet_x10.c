f2cf194f20a52b89275132bd568fef7029c75d9c
theDalton01 <dabeejay691@gmail.com>
2022-06-08 09:57:01 -0700

    nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
  *
- * Return: Always 0
+ * Return: x10 a-z
  */
 void print_alphabet_x10(void)
 {
