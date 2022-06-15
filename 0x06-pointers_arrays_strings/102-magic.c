7a5e1fba01c3473677f225a789baa56080569671
theDalton01 <dabeejay691@gmail.com>
2022-06-15 14:55:51 -0700

    More arrays

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int n;
+	int a[s];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * Line of code goes here
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowd to modify p
+	 *   only one statement
+	 *   you are not allowed to code anything other than this line of code
+	 */
+	p[5] = 98;
+		/* this prints 98/n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
